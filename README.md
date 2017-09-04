54528cdc68898ac075e324f9665102eaf64351bf
David Ding <ding@dingstudio.cn>
2017-09-04 19:45:26 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# csharp_mediaplayer
-CSharp Media Player Demo
+# 基于CSharp开发的WinForm媒体播放器
+一款简单的基于CSharp（.NET WinForm架构）开发的媒体播放器，使用Windows Media Player作为COM核心控件。
+非常简陋，仅供学习使用。。。
+<br>
+<small>&copy; 2017 DingStudio All Rights Reserved | Author: alone◎浅忆</small>
---
+++ WindowsFormsApp/winForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class winForm : Form
+    {
+        public winForm()
+        {
+            InitializeComponent();
+        }
+
+        private void 关于AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("当前系统版本：2017.9.4_alpha");
+        }
+
+        private void 内容CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 索引IToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 搜索SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 自定义CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 选项OToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 撤消UToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 重复RToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 剪切TToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 复制CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 粘贴PToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 全选AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 新建NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 打开OToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //TODO
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false; //禁止选择多个文件
+            dialog.Title = "请选择需要播放的媒体文件"; //设置文件选择框标题
+            dialog.Filter = "所有文件(*.*)|*.*";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string file_path = dialog.FileName;
+                if(MessageBox.Show("您确认要打开媒体文件：" + file_path, "操作确认", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+                {
+                    //MessageBox.Show("启动播放");
+                    axWMP.URL = file_path;
+                    //axWMP.openPlayer(file_path);
+                }
+                else
+                {
+                    file_path = "";
+                }
+            }
+        }
+
+        private void 保存SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 另存为AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 打印PToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 打印预览VToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("开发中的功能，暂未开放。");
+        }
+
+        private void 退出XToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
